7d9b62b2b3fb59a05d64cc003202fbd4e891f0d8
Terraeb <mihalyovvalik@gmail.com>
2024-10-25 14:22:51 +0100

    setting heroku

--- cocktailwiki/cocktailwiki/settings.py
+++ cocktailwiki/cocktailwiki/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+
 from decouple import config
 from django.conf.global_settings import STATICFILES_DIRS
 
@@ -22,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&45x2@^xe=jq#m*h7lifnp7_kwmlvldyt%_n=rr+2r0*q9kyu4'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-&45x2@^xe=jq#m*h7lifnp7_kwmlvldyt%_n=rr+2r0*q9kyu4')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
